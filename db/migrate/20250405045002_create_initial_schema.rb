c851176e7a3a6d125224ce80099aa7fd612c0237
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2025-04-05 13:52:23 +0900

    ci: テストデータベースの準備をdb:migrateに変更し、db:test:prepareを追加

---
+++ db/migrate/20250405045002_create_initial_schema.rb
@@ -0,0 +1,11 @@
+class CreateInitialSchema < ActiveRecord::Migration[7.1]
+  def change
+    # ここに必要なテーブルの定義を追加
+    # 例：
+    # create_table :users do |t|
+    #   t.string :email, null: false
+    #   t.string :password_digest
+    #   t.timestamps
+    # ends
+  end
+end
